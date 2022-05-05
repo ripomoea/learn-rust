149969b1d19214a42854e9012c354ea8d749ba3c
Ryo Takeuchi <develop@r09i.me>
2022-05-05 13:14:03 +0900

    :sparkles: actix-web のネストルーティングサンプル
    actix-web のネストルーティングサンプルコードを追加しました。

---
+++ actix-nested-routing/src/bin/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{middleware, App, HttpServer};
+
+use actix_nested_routing::app_config::config_app;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .configure(config_app)
+            .wrap(middleware::Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
